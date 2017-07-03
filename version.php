143333e055c2d5c8d54d93f4d007167922bcee60
ebarra <enrique.barra.arias@gmail.com>
2017-07-03 11:48:46 +0200

    require moodle changed to be able to install the plugin

--- version.php
+++ version.php
@@ -31,6 +31,6 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->version = 2016010201; // YYYYMMDDVV.
 $plugin->maturity = MATURITY_RC; // This version's maturity level.
 $plugin->release = '3.0.0.2 (Build: 2016010201)';
-$plugin->requires  = 2015111600.00; // 3.0 (Build: 20151116).
+$plugin->requires  = 2015050500.00; // 3.0 (Build: 20151116).
 $plugin->component = 'theme_essential';
 $plugin->dependencies = array();
